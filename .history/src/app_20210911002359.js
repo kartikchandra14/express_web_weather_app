0e4c86a13a503497c29c337fa8639d72816e5c9a
kartikchandra14 <kartikchandra14@users.noreply.github.com>
2021-09-11 23:53:27 +0530

    PORT

--- .history/src/app_20210911002359.js
+++ .history/src/app_20210911002359.js
@@ -3,7 +3,7 @@ const path  = require("path");
 const express = require("express");
 const app = express();
 const hbs = require("hbs");
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 // console.log("path-->", path.join(__dirname.toString(), "../public"));
 const publicDirectoryPath = path.join(__dirname, "../public");
